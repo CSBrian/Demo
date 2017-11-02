b6e1f6d557d59b3a45d51206a45e775a0367878d
dangvan <dangcntp@gmail.com>
2017-11-02 15:56:21 +0700

    origin

---
+++ DialogDemo/DialogDemo/DialogDemo.Android/MainActivity.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace DialogDemo.Droid
+{
+    [Activity(Label = "DialogDemo", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+             global::Xamarin.Forms.Forms.Init(this, bundle);
+            
+            LoadApplication(new App());
+            //---------------
+            //string[] items = {"female","male" };
+            string[] items = { "Hanoi", "Haihong","Nhatrang","Danang","HCM" };
+            int position = 0;
+            AlertDialog.Builder b = new AlertDialog.Builder(this);
+            b.SetTitle("Confirm infor");
+            
+            b.SetItems(items, (s, a) => {//listener
+                Toast.MakeText(this,items[a.Which],ToastLength.Short).Show();  //which 
+            });
+                     
+                //**
+                //b.SetSingleChoiceItems(items, 0, (s, a) => {
+                //    position = a.Which;  
+                //});
+                //b.SetPositiveButton("select", (s, a) => {
+                //    Toast.MakeText(this, items[position], ToastLength.Short).Show();//show success
+                //});
+
+                                
+                //**
+                b.Show();//show popup
+            //**
+            //b.SetMessage("Are you sure!");
+            //b.SetPositiveButton("yes", (s, a) =>
+            //{
+            //    Finish();
+            //});
+            //b.SetNegativeButton("no", (s, a) =>
+            //{
+            //    b.Dispose();
+            //});
+            //AlertDialog ad = b.Create();
+            //b.Show();
+        }
+    }
+
+            
+}
+
---
+++ DialogDemo/DialogDemo/DialogDemo/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DialogDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            //AlertDialog.Builder b = new AlertDialog.Builder();
+        }
+    }
+}
