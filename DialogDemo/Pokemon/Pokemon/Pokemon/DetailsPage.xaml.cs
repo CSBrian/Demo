9e271d2e91ba14992703a8ff9dac044dd6d4c5e2
dangvan <dangcntp@gmail.com>
2017-11-08 14:09:52 +0700

    no message

---
+++ DialogDemo/Pokemon/Pokemon/Pokemon/DetailsPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PokedexNET;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pokemonto
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailsPage : ContentPage
+    {
+        private PokedexNET.Pokemon _pokemonUrl;// new PokedexNET.PokemonInfo();
+        public DetailsPage(PokedexNET.Pokemon pokemon)
+        {
+            InitializeComponent();
+            var _pokedex = new Pokedex();
+            _pokemonUrl = pokemon;
+            var _pokemon = _pokedex.GetPokemonInfo(pokemon);
+            lblName.Text = _pokemon.Names.ToString();
+            lblWeight.Text = _pokemon.WeightEu.ToString();
+            lblHeight.Text = _pokemon.WeightUs.ToString();
+            //imgpoke.Source= ImageSource.FromUri(new Uri(_pokemon.))
+            lblDescription.Text = _pokemon.Evolutions.ToString();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            var url = "http://www.pokemon.com/us/pokedex/" + _pokemonUrl.Slug;
+            Device.OpenUri(new Uri(url));
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
---
+++ DialogDemo/Pokemon/Pokemon/Pokemon/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Android.App;
+using Android.Content;
+using PokedexNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace Pokemonto
+{
+    public partial class MainPage : ContentPage
+    {
+        private Pokedex _pokedex;
+        private PokedexNET.Pokemon _pokemon;
+        //Android.Content.Context context;
+        public MainPage()
+        {
+            InitializeComponent();
+            _pokedex = new Pokedex();
+            _pokemon = new PokedexNET.Pokemon();
+            
+        }
+        private void ButtonFind_Clicked(object sender, EventArgs e)
+        {
+            var text = txtInput.Text;
+            if (string.IsNullOrEmpty(text))
+            {
+                lblName.Text = "Input object name!";
+            }
+
+            else if (int.TryParse(text, out int index))
+            {
+                if (index != 0)
+                {
+                    _pokemon = _pokedex.GetPokemon(index);
+                    lblName.Text = _pokemon.Name;
+                    imgPoke.Source = ImageSource.FromUri(new Uri(_pokemon.ImageUrl));
+                }
+                else
+                {
+                    lblName.Text = "Invalid object!";
+                }
+
+            }
+        }
+        private void btnViewDetail_Clicked(object sender, EventArgs e)
+        {
+            if (_pokemon.Slug!=null)
+            {
+                Navigation.PushAsync(new DetailsPage(_pokemon));
+            }
+        }
+
+        private void btnExit_Clicked(object sender, EventArgs e)
+        {
+            //DisplayAlert("Confirmation", "Are you sure!", "yes", "no");
+            //AlertDialog.Builder builder = new AlertDialog.Builder();
+            //builder.SetTitle("confirmation");
+            //builder.SetPositiveButton("yes", (s, a) =>
+            //{
+            //   //Finish();
+            //});
+            //builder.SetNegativeButton("No", (s, a) =>
+            //{
+            //    builder.Dispose();
+            //});
+            //AlertDialog alert = builder.Create();
+            //alert.Show();
+        }
+    }
+}
